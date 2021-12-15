022d442da5f5566d1902a13d9e3ab81b93591893
bhargavik10 <94848030+bhargavik10@users.noreply.github.com>
2021-12-15 10:35:56 +0530

    Add files via upload

---
+++ quadratic roots using switch.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main()
+{
+	int a,b,c,m,n;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	scanf("%d",&c);
+	m==(-b+(b*b-4*a*c)^1/2)/2*a;
+	n==(+b+(b*b-4*a*c)^1/2)/2*a;
+	switch(m=0)
+	{
+		case 1:printf("%d",m);
+		printf("real and equal");break;
+	}
+	switch(n=0)
+	{
+		case 2:printf("%d",n);
+		printf("real and different");break;
+	}
+
+}
